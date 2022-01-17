5472e633058998ec2de4f0ac11cda5d34a6defdb
Dennis Phillips <dennisphillips@Denniss-MacBook-Pro.local>
2022-01-17 08:42:31 -0500

    updated security

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '48d006aba6604ffab4cf3a3bd59d21ea'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict('f76196b43bbd45c99b4f3cd8e8b40a8a', req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('Unable to work with API'));
+
+};
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get entries.'));
+
+};
+
+module.exports = {
+    handleImage,
+    handleApiCall
+};
--- server.js
+++ server.js
@@ -1,95 +1,41 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors')
+const cors = require('cors');
+const knex = require('knex')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
 
 
 const app = express();
 app.use(cors());
 app.use(express.json())
-const database = {
-    users: [
-            {
-            id: '123',
-            name: 'john',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-            },
-            {
-            id: '124',
-            name: 'jsally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-            }
-        ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
+
 
 app.get('/', (req, res) =>{
-    res.send(database.users)
+    res.send('success');
 })
 
-app.post('/signin', (req, res) =>{
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json(database.users[0]);
-    }else{
-        res.status(400).json('eror loggng in');
-    }
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req, res) =>{
-    const{email, name, password} = req.body;
-    bcrypt.hash(password, null, null, function(err, hash){
-        console.log(hash);
-    })
-    database.users.push({
-        id: '125',
-            name: name,
-            email: email,
-            entries: 0,
-            joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res) =>{
-    const {id} = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id == id){
-            found = true;
-            return res.json(user);
-        }
-    })
-    if(!found) {
-        res.status(400).json('user not found.')
-    }
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 
-app.put('/image', (req, res)=> {
-    const {id} = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id == id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    })
-    if(!found) {
-        res.status(400).json('user not found.')
-    }
-
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
     app.listen(3000, () => {
     console.log('app in running')
